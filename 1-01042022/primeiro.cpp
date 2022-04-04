8e8ec94dc86fb2a86fe995e12dae6bc5ac3e9766
Igor Rabelo <iarsec02@protonmail.com>
2022-04-04 08:29:31 -0300

    Primeiro programa em C++

---
+++ 1-01042022/primeiro.cpp
@@ -0,0 +1,15 @@
+//Programa para exibir uma mensagem.
+//O código foi escrito em C++
+
+//importação da biblioteca de entrada e saida
+
+#include <iostream>
+
+int main(){
+
+    //saida padrão de dados
+    std::cout << "Bem vindo ao C++\n";
+
+    return 0;
+
+}
