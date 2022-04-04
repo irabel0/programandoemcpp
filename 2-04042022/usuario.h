9028c82fbdf80d304d4a0b15fa3bd07ff5f840f1
Igor Rabelo <iarsec02@protonmail.com>
2022-04-04 11:19:59 -0300

    Classe dos Usuarios

---
+++ 2-04042022/usuario.h
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using std::cout;
+using std::cin;
+using std::string;
+
+class Usuario{
+
+    public:
+        string username;
+        string password;
+        string email;
+        string level;
+
+};
